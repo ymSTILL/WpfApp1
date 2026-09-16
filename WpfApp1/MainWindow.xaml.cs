e91c0ddee59e10c2b5cef68dc9453304d15b99cb
owner <owner@C301-20>
2026-09-16 13:11:37 +0800

    加入專案檔案。

---
+++ WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System.Windows;
+
+namespace _2026_WpfApp1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void okButton_Click(object sender, RoutedEventArgs e)
+        {
+            string name = nameTextBox.Text;
+            string telphone = telTextBox.Text;
+            string message = $"Name: {name}\nTelphone: {telphone}";
+            MessageBox.Show(message, "我的資訊", MessageBoxButton.OK);
+        }
+
+        private void computeButton_Click(object sender, RoutedEventArgs e)
+        {
+            int n = int.Parse(numberTextBox.Text);
+
+            string result = "";
+            for (int i = 1; i <= n; i++)
+            {
+                for (int j = 1; j <= n; j++)
+                    result += $"{i}*{j}={i * j}\t\t";
+                result += "\n";
+            }
+            resultTextBlock.Text = result;
+        }
+    }
+}
